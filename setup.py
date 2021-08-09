ae9f1af9311b3e8fd1eea2209001ba4d6c32321e
Brian Towles <btowles@cloudera.com>
2021-08-09 11:02:07 -0500

    Really fix setup versioning for PEP 440 Version

--- setup.py
+++ setup.py
@@ -51,12 +51,43 @@ install_requires = []
 dependency_links = []
 package_data = {}
 
+VERSION_PATTERN = r"""
+__version__\s=\s\"(?P<version>v?
+(?:
+    (?:(?P<epoch>[0-9]+)!)?                           # epoch
+    (?P<release>[0-9]+(?:\.[0-9]+)*)                  # release segment
+    (?P<pre>                                          # pre-release
+        [-_\.]?
+        (?P<pre_l>(a|b|c|rc|alpha|beta|pre|preview))
+        [-_\.]?
+        (?P<pre_n>[0-9]+)?
+    )?
+    (?P<post>                                         # post release
+        (?:-(?P<post_n1>[0-9]+))
+        |
+        (?:
+            [-_\.]?
+            (?P<post_l>post|rev|r)
+            [-_\.]?
+            (?P<post_n2>[0-9]+)?
+        )
+    )?
+    (?P<dev>                                          # dev release
+        [-_\.]?
+        (?P<dev_l>dev)
+        [-_\.]?
+        (?P<dev_n>[0-9]+)?
+    )?
+)
+(?:\+(?P<local>[a-z0-9]+(?:[-_\.][a-z0-9]+)*))?       # local version
+)\"
+"""
+
 
 def get_version() -> Any:
     """Get current version from code."""
-    regex = r"__version__\s=\s\"(?P<version>(\d+!)?(\d+)(\.\d+)+([\.\-\_])?((a(lpha)?|b(eta)?|c|r(c|ev)?|pre(view)?)\d*)?(\.?(post|dev)\d*)?)\""
     path = ("cdpctl", "__version__.py")
-    return re.search(regex, read(*path)).group("version")
+    return re.search(VERSION_PATTERN, read(*path)).group("version")
 
 
 # determine requirements
