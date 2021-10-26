0c4fd2e6e3c3d055b62189ab69b24b2efd75ee30
Brian Towles <btowles@cloudera.com>
2021-10-26 15:26:42 -0500

    fixes for check_for_actions
    Signed-off-by: Brian Towles <btowles@cloudera.com>

--- cdpctl/validation/infra/conftest.py
+++ cdpctl/validation/infra/conftest.py
@@ -343,6 +343,7 @@ def azure_data_required_data_actions() -> List[str]:
 def azure_ranger_audit_required_actions() -> List[str]:
     """Get the Azure actions needed for the ranger audit identity."""
     return [
+        "Microsoft.Storage/storageAccounts/blobServices/write",
         "Microsoft.Storage/storageAccounts/blobServices/containers/write",
     ]
 
@@ -357,6 +358,7 @@ def azure_ranger_audit_required_data_actions() -> List[str]:
 def azure_logger_required_actions() -> List[str]:
     """Get the Azure actions needed for the logger identity."""
     return [
+        "Microsoft.Storage/storageAccounts/blobServices/write",
         "Microsoft.Storage/storageAccounts/blobServices/containers/write",
     ]
 
