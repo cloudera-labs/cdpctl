9979e65ab0e9a1b5f2494aad1b3a7e5d549a084d
Mahendra Korepu <mkorepu@cloudera.com>
2021-10-28 14:20:17 -0500

    CDPCAM-81 Update DataLake Admin role actions validations (#53)
    * CDPCAM-81 Update DataLake Admin Identity actions validations
    
    Signed-off-by: Mahendra Korepu <mkorepu@cloudera.com>

--- cdpctl/validation/infra/validate_azure_dladmin_identity.py
+++ cdpctl/validation/infra/validate_azure_dladmin_identity.py
@@ -40,7 +40,7 @@
 # Source File Name:  validate_azure_dladmin_identity.py
 ###
 """Validation of Azure Datalake Admin Identity."""
-from typing import Any, Dict
+from typing import Any, Dict, List
 
 import pytest
 from azure.mgmt.authorization import AuthorizationManagementClient
@@ -48,13 +48,16 @@ from azure.mgmt.resource import ResourceManagementClient
 
 from cdpctl.validation import fail, get_config_value
 from cdpctl.validation.azure_utils import (
-    check_for_role,
+    check_for_actions,
     get_client,
     get_role_assignments,
     get_storage_container_scope,
     parse_adls_path,
 )
-from cdpctl.validation.infra.issues import AZURE_IDENTITY_MISSING_ROLE
+from cdpctl.validation.infra.issues import (
+    AZURE_IDENTITY_MISSING_ACTIONS_FOR_LOCATION,
+    AZURE_IDENTITY_MISSING_DATA_ACTIONS_FOR_LOCATION,
+)
 
 
 @pytest.fixture(autouse=True, name="resource_client")
@@ -71,13 +74,28 @@ def auth_client_fixture(config: Dict[str, Any]) -> AuthorizationManagementClient
 
 @pytest.mark.azure
 @pytest.mark.infra
-def azure_dladmin_logs_owner_validation(
+def azure_dladmin_actions_for_logs_storage_validation(
     config: Dict[str, Any],
     auth_client: AuthorizationManagementClient,
     resource_client: ResourceManagementClient,
+    azure_data_required_actions,
 ) -> None:  # pragma: no cover
-    """Datalake Admin Identity has the Logs Container Owner Role."""  # noqa: D401,E501
+    """Datalake Admin Identity has required Actions on logs storage location."""  # noqa: D401,E501
+    _azure_dladmin_logs_storage_actions_check(
+        config=config,
+        auth_client=auth_client,
+        resource_client=resource_client,
+        azure_data_required_actions=azure_data_required_actions,
+    )
 
+
+def _azure_dladmin_logs_storage_actions_check(
+    config: Dict[str, Any],
+    auth_client: AuthorizationManagementClient,
+    resource_client: ResourceManagementClient,
+    azure_data_required_actions: List[str],
+) -> None:  # pragma: no cover
+    # noqa: D401,E501
     sub_id: str = get_config_value(config=config, key="infra:azure:subscription_id")
     rg_name: str = get_config_value(config=config, key="infra:azure:metagroup:name")
     storage_name: str = get_config_value(config=config, key="env:azure:storage:name")
@@ -100,33 +118,115 @@ def azure_dladmin_logs_owner_validation(
     proper_scope = get_storage_container_scope(
         sub_id, rg_name, storage_name, container_name
     )
-    proper_role = "Storage Blob Data Owner"
 
-    if not check_for_role(
+    missing_actions, _ = check_for_actions(
         auth_client=auth_client,
         role_assigments=role_assignments,
-        proper_role=proper_role,
         proper_scope=proper_scope,
-    ):
+        required_actions=azure_data_required_actions,
+        required_data_actions=[],
+    )
+
+    if missing_actions:
         fail(
-            AZURE_IDENTITY_MISSING_ROLE,
+            AZURE_IDENTITY_MISSING_ACTIONS_FOR_LOCATION,
             subjects=[
                 datalake_admin,
-                proper_role,
                 f"storageAccounts/{storage_name}/blobServices/default/containers/{container_name}",  # noqa: E501
             ],
+            resources=missing_actions,
         )
 
 
 @pytest.mark.azure
 @pytest.mark.infra
-def azure_dladmin_data_owner_validation(
+def azure_dladmin_data_actions_for_logs_storage_validation(
     config: Dict[str, Any],
     auth_client: AuthorizationManagementClient,
     resource_client: ResourceManagementClient,
+    azure_data_required_data_actions,
 ) -> None:  # pragma: no cover
-    """Datalake Admin Identity has the Storage Container Owner Role."""  # noqa: D401,E501
+    """Datalake Admin Identity has required Data Actions on logs storage location."""  # noqa: D401,E501
+    _azure_dladmin_logs_storage_data_actions_check(
+        config=config,
+        auth_client=auth_client,
+        resource_client=resource_client,
+        azure_data_required_data_actions=azure_data_required_data_actions,
+    )
 
+
+def _azure_dladmin_logs_storage_data_actions_check(
+    config: Dict[str, Any],
+    auth_client: AuthorizationManagementClient,
+    resource_client: ResourceManagementClient,
+    azure_data_required_data_actions: List[str],
+) -> None:  # pragma: no cover
+    # noqa: D401,E501
+    sub_id: str = get_config_value(config=config, key="infra:azure:subscription_id")
+    rg_name: str = get_config_value(config=config, key="infra:azure:metagroup:name")
+    storage_name: str = get_config_value(config=config, key="env:azure:storage:name")
+    log_path: str = get_config_value(config=config, key="env:azure:storage:path:logs")
+    datalake_admin: str = get_config_value(
+        config=config, key="env:azure:role:name:datalake_admin"
+    )
+
+    parsed_logger_path = parse_adls_path(log_path)
+    container_name = parsed_logger_path[1]
+
+    role_assignments = get_role_assignments(
+        auth_client=auth_client,
+        resource_client=resource_client,
+        identity_name=datalake_admin,
+        subscription_id=sub_id,
+        resource_group=rg_name,
+    )
+
+    proper_scope = get_storage_container_scope(
+        sub_id, rg_name, storage_name, container_name
+    )
+
+    _, missing_data_actions = check_for_actions(
+        auth_client=auth_client,
+        role_assigments=role_assignments,
+        proper_scope=proper_scope,
+        required_actions=[],
+        required_data_actions=azure_data_required_data_actions,
+    )
+    if missing_data_actions:
+        fail(
+            AZURE_IDENTITY_MISSING_DATA_ACTIONS_FOR_LOCATION,
+            subjects=[
+                datalake_admin,
+                f"storageAccounts/{storage_name}/blobServices/default/containers/{container_name}",  # noqa: E501
+            ],
+            resources=missing_data_actions,
+        )
+
+
+@pytest.mark.azure
+@pytest.mark.infra
+def azure_dladmin_actions_for_data_storage_validation(
+    config: Dict[str, Any],
+    auth_client: AuthorizationManagementClient,
+    resource_client: ResourceManagementClient,
+    azure_data_required_actions,
+) -> None:  # pragma: no cover
+    """Datalake Admin Identity has required Actions on data storage location."""  # noqa: D401,E501
+    _azure_dladmin_data_storage_actions_check(
+        config=config,
+        auth_client=auth_client,
+        resource_client=resource_client,
+        azure_data_required_actions=azure_data_required_actions,
+    )
+
+
+def _azure_dladmin_data_storage_actions_check(
+    config: Dict[str, Any],
+    auth_client: AuthorizationManagementClient,
+    resource_client: ResourceManagementClient,
+    azure_data_required_actions: List[str],
+) -> None:  # pragma: no cover
+    # noqa: D401,E501
     sub_id: str = get_config_value(config=config, key="infra:azure:subscription_id")
     rg_name: str = get_config_value(config=config, key="infra:azure:metagroup:name")
     storage_name: str = get_config_value(config=config, key="env:azure:storage:name")
@@ -149,19 +249,86 @@ def azure_dladmin_data_owner_validation(
     proper_scope = get_storage_container_scope(
         sub_id, rg_name, storage_name, container_name
     )
-    proper_role = "Storage Blob Data Owner"
 
-    if not check_for_role(
+    missing_actions, _ = check_for_actions(
         auth_client=auth_client,
         role_assigments=role_assignments,
-        proper_role=proper_role,
         proper_scope=proper_scope,
-    ):
+        required_actions=azure_data_required_actions,
+        required_data_actions=[],
+    )
+
+    if missing_actions:
         fail(
-            AZURE_IDENTITY_MISSING_ROLE,
+            AZURE_IDENTITY_MISSING_ACTIONS_FOR_LOCATION,
             subjects=[
                 datalake_admin,
-                proper_role,
                 f"storageAccounts/{storage_name}/blobServices/default/containers/{container_name}",  # noqa: E501
             ],
+            resources=missing_actions,
+        )
+
+
+@pytest.mark.azure
+@pytest.mark.infra
+def azure_dladmin_data_actions_for_data_storage_validation(
+    config: Dict[str, Any],
+    auth_client: AuthorizationManagementClient,
+    resource_client: ResourceManagementClient,
+    azure_data_required_data_actions,
+) -> None:  # pragma: no cover
+    """Datalake Admin Identity has required Data Actions on data storage location."""  # noqa: D401,E501
+    _azure_dladmin_data_storage_data_actions_check(
+        config=config,
+        auth_client=auth_client,
+        resource_client=resource_client,
+        azure_data_required_data_actions=azure_data_required_data_actions,
+    )
+
+
+def _azure_dladmin_data_storage_data_actions_check(
+    config: Dict[str, Any],
+    auth_client: AuthorizationManagementClient,
+    resource_client: ResourceManagementClient,
+    azure_data_required_data_actions: List[str],
+) -> None:  # pragma: no cover
+    # noqa: D401,E501
+    sub_id: str = get_config_value(config=config, key="infra:azure:subscription_id")
+    rg_name: str = get_config_value(config=config, key="infra:azure:metagroup:name")
+    storage_name: str = get_config_value(config=config, key="env:azure:storage:name")
+    data_path: str = get_config_value(config=config, key="env:azure:storage:path:data")
+    datalake_admin: str = get_config_value(
+        config=config, key="env:azure:role:name:datalake_admin"
+    )
+
+    parsed_data_path = parse_adls_path(data_path)
+    container_name = parsed_data_path[1]
+
+    role_assignments = get_role_assignments(
+        auth_client=auth_client,
+        resource_client=resource_client,
+        identity_name=datalake_admin,
+        subscription_id=sub_id,
+        resource_group=rg_name,
+    )
+
+    proper_scope = get_storage_container_scope(
+        sub_id, rg_name, storage_name, container_name
+    )
+
+    _, missing_data_actions = check_for_actions(
+        auth_client=auth_client,
+        role_assigments=role_assignments,
+        proper_scope=proper_scope,
+        required_actions=[],
+        required_data_actions=azure_data_required_data_actions,
+    )
+    if missing_data_actions:
+        fail(
+            AZURE_IDENTITY_MISSING_DATA_ACTIONS_FOR_LOCATION,
+            subjects=[
+                datalake_admin,
+                f"storageAccounts/{storage_name}/blobServices/default/containers/{container_name}",  # noqa: E501
+            ],
+            resources=missing_data_actions,
         )
